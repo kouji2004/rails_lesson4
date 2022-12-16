810e0a313154cf41fa64b761d1063dc5ce05a5da
kouji2004 <mrtaxi48@gmail.com>
2022-12-16 10:29:11 +0000

    投稿一覧ページの作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,12 +5,13 @@ class PostImagesController < ApplicationController
 
   def create
     @post_image = PostImage.new(post_image_params)
-    @post_image.users_id = current_user.id
+    @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
