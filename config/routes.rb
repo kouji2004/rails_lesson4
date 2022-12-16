61820582fc3d8a410edefc002c8469d1e6a15cc3
kouji2004 <mrtaxi48@gmail.com>
2022-12-16 12:05:51 +0000

    投稿に対してのコメント機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about"=>"homes#about"
   resources :users, only: [:show,:edit, :update]
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
